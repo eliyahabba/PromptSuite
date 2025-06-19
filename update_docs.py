7335ef2660f456fb530052a4e8a8f2b9ed71691f
ehabba <ehabba@lightricks.com>
2025-06-20 01:40:46 +0300

    chore: update_docs.py utility script changes

--- update_docs.py
+++ update_docs.py
@@ -1,28 +1,28 @@
 import re
 from pathlib import Path
-from src.multipromptify.template_keys import (
+from src.multipromptify.core.template_keys import (
     INSTRUCTION_TEMPLATE_KEY, INSTRUCTION_KEY, QUESTION_KEY, GOLD_KEY, FEW_SHOT_KEY, OPTIONS_KEY, CONTEXT_KEY, PROBLEM_KEY,
     GOLD_FIELD, INSTRUCTION_TEMPLATE_FIELD,
     PARAPHRASE_WITH_LLM, REWORDING, CONTEXT_VARIATION, SHUFFLE_VARIATION, MULTIDOC_VARIATION, ENUMERATE_VARIATION
 )
 
 REPLACEMENTS = {
-    # 'instruction_template': INSTRUCTION_TEMPLATE_KEY,
-    # 'instruction': INSTRUCTION_KEY,
-    # 'question': QUESTION_KEY,
-    # 'gold': GOLD_KEY,
-    # 'few_shot': FEW_SHOT_KEY,
-    # 'options': OPTIONS_KEY,
-    # 'context': CONTEXT_KEY,
-    # 'problem': PROBLEM_KEY,
-    # 'gold_field': GOLD_FIELD,
-    # 'instruction_template_field': INSTRUCTION_TEMPLATE_FIELD,
+    'instruction_template': INSTRUCTION_TEMPLATE_KEY,
+    'instruction': INSTRUCTION_KEY,
+    'question': QUESTION_KEY,
+    'gold': GOLD_KEY,
+    'few_shot': FEW_SHOT_KEY,
+    'options': OPTIONS_KEY,
+    'context': CONTEXT_KEY,
+    'problem': PROBLEM_KEY,
+    'gold_field': GOLD_FIELD,
+    'instruction_template_field': INSTRUCTION_TEMPLATE_FIELD,
     'paraphrase': PARAPHRASE_WITH_LLM,
-    # 'surface': REWORDING,
-    # 'context_variation': CONTEXT_VARIATION,
-    # 'shuffle': SHUFFLE_VARIATION,
-    # 'multidoc': MULTIDOC_VARIATION,
-    # 'enumerate': ENUMERATE_VARIATION,
+    'surface': REWORDING,
+    'context_variation': CONTEXT_VARIATION,
+    'shuffle': SHUFFLE_VARIATION,
+    'multidoc': MULTIDOC_VARIATION,
+    'enumerate': ENUMERATE_VARIATION,
 }
 
 DOCS_ROOTS = [
