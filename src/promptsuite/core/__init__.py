7ea7aecb79c44060c4bbf184878ba218654932b2
ehabba <ehabba@lightricks.com>
2025-07-08 00:52:20 +0300

    update version

--- src/promptsuite/core/__init__.py
+++ src/promptsuite/core/__init__.py
@@ -2,7 +2,7 @@
 PromptSuiteEngine: A tool that creates multi-prompt datasets from single-prompt datasets using templates.
 """
 
-__version__ = "3.0.4"
+__version__ = "3.0.5"
 
 # Import main classes for easier access
 from .engine import PromptSuiteEngine
